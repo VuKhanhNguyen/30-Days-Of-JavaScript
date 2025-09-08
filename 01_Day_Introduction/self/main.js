e976847322addd2fece372cb0cca1cf62eb7573d
khanhnguvcl <nguyenvukhanh392@gmail.com>
2025-09-08 16:46:46 +0700

    day 1 is done

---
+++ 01_Day_Introduction/self/main.js
@@ -0,0 +1,7 @@
+console.log(firstname, lastname, country, city, age, isMarried)
+console.log(gravity, boiling_Point, PI)
+console.log(name, job, live)
+
+document.getElementById("show").onclick = function(){
+    alert(firstname + " " + lastname + "\n" + country + "\n" + city + "\n" + age + "\n" + isMarried + "\n\n" + gravity + "\n" + boiling_Point + "\n" + PI + "\n\n" + name + "\n" + job + "\n" + live)
+}
---
+++ 01_Day_Introduction/self/variable.js
@@ -0,0 +1,12 @@
+let firstname = 'Vũ Khanh'
+let lastname = 'Nguyễn'
+let country = 'Việt Nam'
+let city = 'Đà Nẵng'
+let age = 21
+let isMarried = false
+
+const gravity = 9.81
+const boiling_Point = 100
+const PI = 3.14
+
+let name = 'Vũ Khanh', job = 'Student', live = 'Đà Nẵng'
